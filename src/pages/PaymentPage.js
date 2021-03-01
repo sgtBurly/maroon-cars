4e78e0d064026ab22fc7d8df5adabed4cc1cc7d0
Johan Markström <johan.markstrom@gmail.com>
2021-03-01 13:35:06 +0100

    added user form to compoent

--- src/pages/PaymentPage.js
+++ src/pages/PaymentPage.js
@@ -1,7 +1,19 @@
 const PaymentPage = () => {
     return ( 
-        <div className="PAymentPage">
-            
+        <div className="PaymentPage">
+            <section className="SummaryWrapper">
+
+            </section>
+            <section className="FormWrapper">
+                <form>
+                    <input type="text" placeholder="First name..."/>
+                    <input type="text" placeholder="Last name..."/>
+                    <input type="text" placeholder="Address..."/>
+                    <input type="text" placeholder="City..."/>
+                    <input type="text" placeholder="Zip code..."/>
+                    <input type="text" placeholder="State..."/>
+                </form>
+            </section>
         </div>
      );
 }
