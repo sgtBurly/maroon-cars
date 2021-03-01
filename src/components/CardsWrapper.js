e4ca0e1f6f58da6bddda6efe9bff7babe6085d95
frida stenberg <fridastenberg97@hotmail.com>
2021-03-01 14:52:12 +0100

    added map to loop cardCards

--- src/components/CardsWrapper.js
+++ src/components/CardsWrapper.js
@@ -1,9 +1,12 @@
-import { CarCards } from "./CarCards";
+import { CarCard } from "./CarCard";
 
 const CardWrapper = () => {
     return (
+        //looping carCards
         <div className="card-wrapper">
-
+            {CarCard.map((card, i) => (
+                <CarCard card={card} key={i} />
+            ))}
         </div>
     )
 }
