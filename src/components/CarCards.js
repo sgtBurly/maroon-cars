ce3c4c581ad8f5910507d29fa09bc9729cd42eb7
frida stenberg <fridastenberg97@hotmail.com>
2021-03-01 13:40:11 +0100

    created carCards component

---
+++ src/components/CarCards.js
@@ -0,0 +1,12 @@
+//importing for future use
+import { CarContext } from "../contexts/CarContext";
+
+const CarCards = () => {
+    return (
+        <div className="car-cards">
+
+        </div>
+    )
+}
+
+export default CarCards;
