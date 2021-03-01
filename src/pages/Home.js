15a8deeadc5f57fe9f930c5397362dd9254dae07
Cedrik von Heiroth <cedrikvonheiroth@gmail.com>
2021-03-01 15:20:14 +0100

    Delete Navbar component from Home.js
    it does not have to be there

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import Navbar from '../components/Navbar';
+
 
 const Home = () => {
   return (
     <div>
-      <Navbar />
       <h1>This is homepage</h1>
     </div>
   );
