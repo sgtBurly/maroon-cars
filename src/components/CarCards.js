a6629bb722d25db029c0e764ff775fe660dda608
frida stenberg <fridastenberg97@hotmail.com>
2021-03-01 14:16:56 +0100

    added bootstrap card

--- src/components/CarCards.js
+++ src/components/CarCards.js
@@ -1,10 +1,20 @@
-//importing for future use
 import { CarContext } from "../contexts/CarContext";
+import { assets } from "../../public/assets/car-pictures";
+
+//importing bootstrap, not sure if correct
+import Card from 'react-bootstrap/Card';
 
 const CarCards = () => {
+// Possible variables from carContext
+const { carInfo } = useContext(CarContext);
     return (
-        <div className="car-cards">
-
+        <div className="card" style="width 18rem;">
+            <img className="card-img-top" src="..." alt="Card image cap"/>
+            <div className="card-body">
+                <h5 className="card-title">Card Title</h5>
+                <p className="card-text">Some Text blblabla</p>
+                <a href="#" className="btn btn-primary">Go somewhere</a>
+            </div>
         </div>
     )
 }
