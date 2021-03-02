60bca3990219f2b0487d941fcf0b7021bb8de785
frida stenberg <fridastenberg97@hotmail.com>
2021-03-02 13:53:51 +0100

    changed path

--- src/components/CarCard.js
+++ src/components/CarCard.js
@@ -7,7 +7,7 @@ function CarCard(props) {
 
     return (
         <div className="card" style="width 18rem;">
-            <img className="card-img-top" src={`{"../assets/car-pictures/${props.data.make}-${props.data.model}-${props.data.year}.jpg}`} alt={props.data.model}/>
+            <img className="card-img-top" src="`../assets/car-pictures/${props.data.make}-${props.data.model}-${props.data.year}.jpg`" alt={props.data.model}/>
             <div className="card-body">
                 <h5 className="card-title">{props.data.make}</h5>
                 <p className="card-text">{props.data.price}</p>
