69241b85a1269c89817da9dae78285151d73f3dc
frida stenberg <fridastenberg97@hotmail.com>
2021-03-02 12:52:14 +0100

    added props to carCard element

--- src/components/CarCard.js
+++ src/components/CarCard.js
@@ -1,15 +1,17 @@
 //importing bootstrap, not sure if correct
 import Card from 'react-bootstrap/Card';
 
-//receiving props from parent CardsWrapper 
+//receiving props from parent CardsWrapper
 function CarCard(props) {
+
 
     return (
         <div className="card" style="width 18rem;">
-            <img className="card-img-top" src="..." alt="Card image cap"/>
+            <img className="card-img-top" src={`{"../assets/car-pictures/${props.data.make}-${props.data.model}-${props.data.year}.jpg}`} alt={props.data.model}/>
             <div className="card-body">
-                <h5 className="card-title">Card Title</h5>
-                <p className="card-text">Some Text blblabla</p>
+                <h5 className="card-title">{props.data.make}</h5>
+                <p className="card-text">{props.data.price}</p>
+                <p className="card-text">{props.data.descShort}</p>
                 <a href="#" className="btn btn-primary">Go somewhere</a>
             </div>
         </div>
