2ebb36403790ceae56354ddd197d18a9b9639658
Christina <chrasse@hotmail.com>
2021-03-02 12:49:21 +0100

    Changed CarContext to use require() to get json-data

--- src/contexts/CarContext.js
+++ src/contexts/CarContext.js
@@ -1,11 +1,15 @@
-import { createContext, useState } from 'react'
-import data from '../json/cars.json'
+import { createContext, useState, useEffect } from 'react'
+// import data from '../json/cars.json'
 
 export const CarContext = createContext();
 
 const CarContextProvider = (props) => {
 
-  const [cars, setCars] = useState(data);
+  const [cars, setCars] = useState([]);
+
+  useEffect(() => {
+    setCars(require("../json/cars.json"));
+  }, []);
 
   const values = {
     cars
