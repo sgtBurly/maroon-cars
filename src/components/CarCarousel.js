07e0b9c8c0970629079b761a98f681c08df21308
Johan Markström <johan.markstrom@gmail.com>
2021-03-02 16:05:26 +0100

    continued work on Carousel. NOTE! minor bugs.

--- src/components/CarCarousel.js
+++ src/components/CarCarousel.js
@@ -1,8 +1,27 @@
+import Carousel from 'react-bootstrap/Carousel'
+import CarContext from '../contexts/CarContext';
+import React, { useContext } from 'react';
+
 const CarCarousel = () => {
+
+    const Cars = useContext(CarContext);
+
+    const renderCarousel = () => {
+        Cars.map(() => {
+            <Carousel>
+                <Carousel.Item>
+                    <Carousel.Caption>
+                    <h3>`${Cars.make}`</h3>
+                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+            </Carousel>
+        })
+    }
 
     return ( 
         <div className="test">
-            test
+            {renderCarousel(Cars)}
         </div>
      );
 }
