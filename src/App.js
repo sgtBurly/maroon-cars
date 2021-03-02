00dac12bf08ce7d4724d161df9666e9a55c9ea95
Johan Markström <johan.markstrom@gmail.com>
2021-03-02 14:06:58 +0100

    removed unimportant code + bugfix cleared.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import React from 'react';
-import { BasketProvider } from './contexts/BasketContext';
 import Home from './pages/Home';
 import About from './pages/About';
-import PaymentPage from "./pages/PaymentPage"
 import Navbar from './components/Navbar.js'
 import { BrowserRouter, Route } from 'react-router-dom'
 import BasketProvider from './contexts/BasketContext.js';
-import Home from './pages/Home'
-import About from './pages/About'
 
 
 function App() {
