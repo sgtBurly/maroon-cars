b19c7e61ae7cb8e86ac4bce8ae8512bbb304e450
Christina <chrasse@hotmail.com>
2021-03-03 14:56:01 +0100

    Added customerBasket and addToBAsket-method in BasketContext

--- src/contexts/BasketContext.js
+++ src/contexts/BasketContext.js
@@ -4,9 +4,18 @@ export const BasketContext = createContext();
 
 export const BasketProvider = (props) => {
 
+    // The cart holding the array with the "saved" cars
+    const [customerBasket, setCostumerBasket] = useState([]);
+
+    // method to call by the "add to cart"-buttons
+    const addToBasket = car => {
+        console.log('This was added to your cart: ', car)
+        setCostumerBasket(prevState => [car, ...prevState]);
+    }
 
     const values = {
-
+        customerBasket,
+        addToBasket
     }
 
 return (
