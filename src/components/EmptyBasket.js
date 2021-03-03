08d29f2c04174f8a921c8f9f137ca77c7b75b6c5
frida stenberg <fridastenberg97@hotmail.com>
2021-03-03 10:20:05 +0100

    created component for empty basket and css

---
+++ src/components/EmptyBasket.js
@@ -0,0 +1,11 @@
+import EmptyBasketStyles from "../styles/EmptyBasketStyles.css"
+
+function EmptyBasket() {
+    return(
+        <div className={EmptyBasketStyles.empty}>
+
+        </div>
+    )
+}
+
+export default EmptyBasket;
