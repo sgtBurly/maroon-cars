13b8ce22e10772b1dfa4aafb16d70d4461d0fd23
August <augustraro112@hotmail.com>
2021-03-03 12:00:14 +0100

    Added some ugly styling to the ReceiptPage

--- src/pages/OrderReceipt.js
+++ src/pages/OrderReceipt.js
@@ -2,12 +2,25 @@ import React from 'react';
 import ReceiptStyles from '../styles/ReceiptStyles.module.css'
 
 const orderReceipt = () => {
+  let today = new Date();
+  let dd = String(today.getDate()).padStart(2, '0');
+  let mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+  let yyyy = today.getFullYear();
+  today = mm + '/' + dd + '/' + yyyy;
+  //window.print();
+
   return (
     <div className={ReceiptStyles.main_wrapper}>
-      <div>
-        
-      </div>
+    <div className={ReceiptStyles.info_card}>
+      <h4>Receipt of purchase</h4>
+      <p className={ReceiptStyles.p}>Model:<span>{/* Put model-data here */}</span></p>
+      <p className={ReceiptStyles.p}>Make:<span>{/* Put make-data here */}</span></p>
+      <p className={ReceiptStyles.p}>Year:<span>{/* Put year-data here */}</span></p>
+      <p className={ReceiptStyles.p}>VIN:<span>{/* Put vin-data here */}</span></p>
+      <p className={ReceiptStyles.p}>Price:<span>{/* Put price-data here */}</span></p>
+      <p className={ReceiptStyles.date}>Purchased made: {today}</p>
     </div>
+  </div>
   );
 }
 
