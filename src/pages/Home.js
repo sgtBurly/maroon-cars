899897bcf63d392bf590dec802ed2b660ff51104
Cedrik von Heiroth <cedrikvonheiroth@gmail.com>
2021-03-03 14:12:58 +0100

    Added welcome message to hero

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,6 +5,9 @@ const Home = () => {
   return (
     <div>
       <div className={style.imageContainer}>
+        <div className={style.greeting}>
+          <h2>Welcome to Maroon Cars<br></br><span>Find the perfect car for you!</span></h2>
+        </div>
         <img src="/assets/hero-image.jpg" alt="car in sunset"/>
       </div>
     </div>
