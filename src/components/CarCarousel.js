534b0c512ac7d2a3933afdf75f4f298278ca3133
Johan Markström <johan.markstrom@gmail.com>
2021-03-03 10:27:53 +0100

    Added script tag in indexHTML changed JSx
     Changed how to output carousel with JSx.

--- src/components/CarCarousel.js
+++ src/components/CarCarousel.js
@@ -4,24 +4,35 @@ import React, { useContext } from 'react';
 
 const CarCarousel = () => {
 
-    const Cars = useContext(CarContext);
-
-    const renderCarousel = () => {
-        Cars.map(() => {
-            <Carousel>
-                <Carousel.Item>
-                    <Carousel.Caption>
-                    <h3>`${Cars.make}`</h3>
-                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-            </Carousel>
-        })
-    }
 
     return ( 
         <div className="test">
-            {renderCarousel(Cars)}
+            <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                <ol class="carousel-indicators">
+                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                </ol>
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                    <img class="d-block w-100" src="..." alt="First slide"></img>
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="..." alt="Second slide"></img>
+                </div>
+                <div class="carousel-item">
+                <img class="d-block w-100" src="..." alt="Third slide"></img>
+                </div>
+                </div>
+                <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
+            </div>
         </div>
      );
 }
