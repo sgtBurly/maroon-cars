e750aed556d0230c19057091ed0df90a1f250da1
frida stenberg <fridastenberg97@hotmail.com>
2021-03-04 09:01:11 +0100

    added func to go to details page

--- src/components/CarCard.js
+++ src/components/CarCard.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import CarCardStyles from '../styles/CarCardStyles.module.css';
+import { useHistory } from "react-router-dom";
 
 //receiving props from parent CardsWrapper
 function CarCard(props) {
 
+//Saving the history hook in a variable
+const historyHook = useHistory();
+
+const clickToRender = () => {
+    //Using the cars unique vin-number to push to new route with the historyHook variable
+    historyHook.push(`/details/${props.data.vin}`)
+}
+
     return (
         <div className="col-lg-4 col-sm-6 mb-3">
-            <div className={`${CarCardStyles.cardHover} card h-100`}>
+            {/* adding clickToRender functon on carCard */}
+            <div className={`${CarCardStyles.cardHover} card h-100`} onClick={clickToRender}>
                 <img className={`${CarCardStyles.imgHover} card-img-top`} src={`../assets/car-pictures/${props.data.make}-${props.data.model}-${props.data.year}.jpg`} alt={props.data.model}/>
                 <div className="card-body">
                     <h5 className="card-title">{props.data.make} - {props.data.model}</h5>
