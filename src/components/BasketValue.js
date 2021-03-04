05a4ad999c2f13b0274c2bc7d6af0e05f2b95b0a
frida stenberg <fridastenberg97@hotmail.com>
2021-03-04 11:38:37 +0100

    created component for value/price in navbar

---
+++ src/components/BasketValue.js
@@ -0,0 +1,11 @@
+import ValueStyles from "../styles/BasketValueStyles.module.css"
+
+function BasketValue () {
+    return (
+        <div className={ValueStyles.valueWrapper}>
+
+        </div>
+    )
+}
+
+export default BasketValue;
