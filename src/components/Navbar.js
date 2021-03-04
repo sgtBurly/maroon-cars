bc8fbff57eacd25aa12278e0b4cbe0e6eab0c269
Christina <chrasse@hotmail.com>
2021-03-04 10:38:12 +0100

    Made Navbar sticky and cart-icon slightly bigger

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,11 +6,11 @@ import * as ReactBootstrap from "react-bootstrap"
 export default function Navbar() {
 
     return (
-        <div className={styles.navbar}>
-            <ReactBootstrap.Navbar collapseOnSelect className="navbar-fixed-top" expand="sm" variant="light">
-                <ReactBootstrap.Navbar.Brand className={styles.font}>Maroon Cars</ReactBootstrap.Navbar.Brand>
+        // <div className={styles.navbar} sticky="top">
+            <ReactBootstrap.Navbar collapseOnSelect sticky="top" className={`navbar ${styles.navbar}`} expand="sm" variant="light">
+                <ReactBootstrap.Navbar.Brand><span className={styles.font}>Maroon Cars</span></ReactBootstrap.Navbar.Brand>
                 <div className="d-flex">
-                    <ReactBootstrap.Nav.Link className={`${styles.clr} d-sm-none`} href="paymentPage"><i className="fas fa-shopping-cart"></i></ReactBootstrap.Nav.Link>
+                    <ReactBootstrap.Nav.Link className={`${styles.clr} d-sm-none`} href="paymentPage"><i className="fas fa-shopping-cart fa-lg"></i></ReactBootstrap.Nav.Link>
                     <ReactBootstrap.Navbar.Toggle className={`${styles.border} navbar-light`} aria-controls="responsive-navbar-nav">
                         <span className={`navbar-toggler-icon ${styles.toggleButton}`}></span>
                     </ReactBootstrap.Navbar.Toggle>
@@ -19,11 +19,11 @@ export default function Navbar() {
                     <ReactBootstrap.Nav className="ml-auto" >
                         <ReactBootstrap.Nav.Link  className={styles.clr} href="/">Home</ReactBootstrap.Nav.Link>
                         <ReactBootstrap.Nav.Link className={styles.clr} href="/about">About</ReactBootstrap.Nav.Link>
-                        <ReactBootstrap.Nav.Link className={`${styles.clr} d-none d-sm-inline-block`} href="paymentPage"><i className="fas fa-shopping-cart"></i></ReactBootstrap.Nav.Link>
+                        <ReactBootstrap.Nav.Link className={`${styles.clr} d-none d-sm-inline-block`} href="paymentPage"><i className="fas fa-shopping-cart fa-lg"></i></ReactBootstrap.Nav.Link>
                     </ReactBootstrap.Nav>
                 </ReactBootstrap.Navbar.Collapse>
             </ReactBootstrap.Navbar>
-        </div>
+        // </div>
     )
 }
 
