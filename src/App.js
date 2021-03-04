df5342d9683cb2cde738efa7b24b0a3bbeac4107
Mona Khorasani <mona.khorasani@medieinstitutet.se>
2021-03-04 15:53:47 +0100

    importing CardComponent

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { BrowserRouter, Route } from 'react-router-dom'
 import Footer from './components/Footer.js'
 import BasketProvider from './contexts/BasketContext.js';
 import Details from './pages/Details'
+import Card from "./components/Card";
 
 
 function App() {
