dc483c6a614c8b95577d577b90bf6673ea460efe
August <augustraro112@hotmail.com>
2021-03-05 10:34:36 +0100

    Filtered out all the discounted cars

--- src/components/CarCarousel.js
+++ src/components/CarCarousel.js
@@ -1,47 +1,40 @@
 import Carousel from 'react-bootstrap/Carousel'
-import CarContext from '../contexts/CarContext';
+import { CarContext } from '../contexts/CarContext';
 import React, { useContext } from 'react';
-import Button from 'react-bootstrap/Button';
 import CarouselStyles from '../styles/CarCarousel.module.css'
 
 const CarCarousel = () => {
-    return (
-    <div className={CarouselStyles.main_wrapper}>
-    <Carousel className={CarouselStyles.caro_wrapper}>
-    <Carousel.Item className={`${CarouselStyles.carousel_item}`}>
-        <img
-            className={`d-block w-100 ${CarouselStyles.carousel_image}`}
-            src="../assets/car-pictures/Dodge-Ram 3500 Club-1997.jpg"
-            alt="First slide"
-        />
-    <Carousel.Caption className={CarouselStyles.carousel_info}>
-        <img alt="sales icon" src="../assets/sale-icon.png" className={CarouselStyles.sales_icon}/>
-    </Carousel.Caption>
-    </Carousel.Item>
-    <Carousel.Item className={`${CarouselStyles.carousel_item}`}>
-    <img
-      className={`d-block w-100 ${CarouselStyles.carousel_image}`}
-      src="../assets/car-pictures/Audi-80-1989.jpg"
-      alt="Second slide"
-    />
-    <Carousel.Caption className={CarouselStyles.carousel_info}>
-        <img alt="sales icon" src="../assets/sale-icon.png" className={CarouselStyles.sales_icon}/>
-    </Carousel.Caption>
-    </Carousel.Item >
-    <Carousel.Item className={`${CarouselStyles.carousel_item}`}>
-    <img
-      className={`d-block w-100 ${CarouselStyles.carousel_image}`}
-      src="../assets/car-pictures/Ford-Festiva-1993.jpg"
-      alt="Third slide"
-    />
-    <Carousel.Caption className={CarouselStyles.carousel_info}>
-      <img alt="sales icon" src="../assets/sale-icon.png" className={CarouselStyles.sales_icon}/>
-    </Carousel.Caption>
-  </Carousel.Item>
-  </Carousel>
+    const { cars } = useContext(CarContext);
+    //find the cars that are discounted
+    let discountedCars = cars.filter(car => car.discount === true);
+    console.log("This is the discountedCars array " + discountedCars)
 
-</div>
-    );
+
+    return (
+      <div className={CarouselStyles.main_wrapper}>
+      <Carousel className={CarouselStyles.caro_wrapper}>
+       {
+       discountedCars.map(disCar => {
+        return (
+          <Carousel.Item className={`${CarouselStyles.carousel_item}`}>
+          <img
+              className={`d-block w-100 ${CarouselStyles.carousel_image}`}
+              src="../assets/car-pictures/Dodge-Ram 3500 Club-1997.jpg"
+              alt="First slide"
+          />
+      <Carousel.Caption className={CarouselStyles.carousel_info}>
+          <img alt="sales icon" src="../assets/sale-icon.png" className={CarouselStyles.sales_icon}/>
+      </Carousel.Caption>
+      </Carousel.Item>
+        )
+      })}
+
+
+
+      </Carousel>
+
+    </div>
+    )
 }
 
 export default CarCarousel;
