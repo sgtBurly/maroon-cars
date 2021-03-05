3aa45f1e5388f445b50376a29b02eede3593ad50
Cedrik von Heiroth <cedrikvonheiroth@gmail.com>
2021-03-05 13:59:04 +0100

    Render EmptyBasket component when customerBasker.length < 1

--- src/pages/PaymentPage.js
+++ src/pages/PaymentPage.js
@@ -1,14 +1,24 @@
-import React from "react";
+import React, { useContext } from "react";
 import PaymentForm from "../components/PaymentForm";
+import EmptyBasket from "../components/EmptyBasket";
+import { BasketContext } from '../contexts/BasketContext';
+
 
 const PaymentPage = () => {
+    const {customerBasket} = useContext(BasketContext);
+
+    if (customerBasket.length < 1) {
+        return <EmptyBasket />
+    } else {
 
     return (
         <div className="PaymentPage">
             <h1>Payment page</h1>
+            {/* Summary component will go here */}
             <PaymentForm />
         </div>
-     );
+    );
+    }
 }
 
 export default PaymentPage;
