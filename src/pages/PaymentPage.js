671ccabb7ac3c6b3ca0428c1a9627f09c5f64814
August <augustraro112@hotmail.com>
2021-03-05 16:13:19 +0100

    Removed old PaymentPage

--- src/pages/PaymentPage.js
+++ src/pages/PaymentPage.js
@@ -1,30 +1,11 @@
 import React, { useState } from "react";
 const PaymentPage = () => {
 
-    let [ FirstName, setFirstName ] = useState(`First name...`);
-    let [ LastName, setLastName ] = useState(`Last name...`);
-    let [ Address, setAddress ] = useState(`Address...`);
-    let [ City, setCity ] = useState(`City...`);
-    let [ ZipCode, seZipCode ] = useState(`Zip code...`);
-    let [ Country, setCountry ] = useState(`Country...`);
 
-    return ( 
+
+    return (
         <div className="PaymentPage">
-            <section className="SummaryWrapper">
-                <div className="SummaryCard">
-                </div>
-            </section>
-            <section className="FormWrapper">
-                <form>
-                    <input type="text" placeholder={FirstName}/>
-                    <input type="text" placeholder={LastName}/>
-                    <input type="text" placeholder={Address}/>
-                    <input type="text" placeholder={City}/>
-                    <input type="text" placeholder={ZipCode}/>
-                    <input type="text" placeholder={Country}/>
-                </form>
-                <button>Complete Purchase</button>
-            </section>
+
         </div>
      );
 }
