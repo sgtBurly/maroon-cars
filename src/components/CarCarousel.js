dcac147ea04a1b03654acad11a205a2766483acd
Cedrik von Heiroth <cedrikvonheiroth@gmail.com>
2021-03-05 14:20:06 +0100

    cleaning code

--- src/components/CarCarousel.js
+++ src/components/CarCarousel.js
@@ -8,37 +8,24 @@ const CarCarousel = () => {
     const { cars } = useContext(CarContext);
     //find the cars that are discounted
     let discountedCars = cars.filter(car => car.discount === true);
-    console.log("This is the discountedCars array " + discountedCars)
-
 
     return (
       <div className={CarouselStyles.main_wrapper}>
       <Carousel className={CarouselStyles.caro_wrapper}>
-       {
+      {
        discountedCars.map(disCar => {
         return (
-
           <Carousel.Item className={`${CarouselStyles.carousel_item}` }>
-
             <img src={`/assets/car-pictures/${disCar.make}-${disCar.model}-${disCar.year}.jpg`} alt={`discounted car ${disCar.make} ${disCar.model}`} />
-
-
-
           <Carousel.Caption className={CarouselStyles.carousel_info}>
-          <img alt="sales icon" src="../assets/sale-icon.png" className={CarouselStyles.sales_icon}/>
+            <img alt="sales icon" src="../assets/sale-icon.png" className={CarouselStyles.sales_icon}/>
           <Link to={`/details/${disCar.vin}`}>
-            <button className={CarouselStyles.carousel_button}>Buy now!</button>
+            <button className={CarouselStyles.carousel_button}>Read more</button>
           </Link>
-
           </Carousel.Caption>
           </Carousel.Item>
-
-
         )
       })}
-
-
-
       </Carousel>
 
     </div>
