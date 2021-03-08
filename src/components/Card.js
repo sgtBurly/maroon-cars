c52486604f8408e368917c78dca7658786092ba9
frida stenberg <fridastenberg97@hotmail.com>
2021-03-08 11:43:14 +0100

    added a key for card

--- src/components/Card.js
+++ src/components/Card.js
@@ -8,20 +8,20 @@ import styles from "../styles/CardsWrapperStyles.module.css";
 const Card = () => {
     const {customerBasket} = useContext(BasketContext);
 
-    return ( 
+    return (
         <div className={styles.cardWrapperContainer}>
-            <div className={styles.cardWrapper}>  
+            <div className={styles.cardWrapper}>
                 <h2 className={styles.cardHeading}>Current item/s in your basket</h2>
                 {customerBasket.map((car, i) => (
-                    <div className={styles.flex}>
+                    <div className={styles.flex} key={i}>
                         <div className={styles.carImageContainer}>
                             <img className={styles.carImage}
-                                src={`/assets/car-pictures/${car.make}-${car.model}-${car.year}.jpg`} 
-                                alt={`picture of ${car.make} ${car.model}`} 
+                                src={`/assets/car-pictures/${car.make}-${car.model}-${car.year}.jpg`}
+                                alt={`picture of ${car.make} ${car.model}`}
                             />
                         </div>
-                        <div> 
-                            <span>Make: {car.make} Model: {car.model}</span>     
+                        <div>
+                            <span>Make: {car.make} Model: {car.model}</span>
                             <p>Year: {car.year}</p>
                         </div>
                         <p>Short description: {car.descShort}</p>
