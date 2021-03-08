56e9ba1df3e5565a74549357c0898193b34b1902
Cedrik von Heiroth <cedrikvonheiroth@gmail.com>
2021-03-08 10:26:48 +0100

    Display total price of customerBasket in navbar

--- src/components/BasketValue.js
+++ src/components/BasketValue.js
@@ -1,14 +1,15 @@
 import ValueStyles from "../styles/BasketValueStyles.module.css"
-import BasketContext from "../contexts/BasketContext";
+import {BasketContext} from "../contexts/BasketContext";
+import { useContext } from "react"; 
 
 function BasketValue () {
     const { customerBasket, calcBasket } = useContext(BasketContext);
-    let totalPrice = calcBasket;
+    let totalPrice = calcBasket(customerBasket);
 
     return (
         <div>
             {/*Only displaying BasketValue if the costumer have added a car to basket */}
-            {customerBasket &&
+            {customerBasket.length > 0 && 
             <div className={ValueStyles.valueWrapper}>
                 <p className={ValueStyles.count}>{customerBasket.length}</p>
                 <p className={ValueStyles.price}>${totalPrice}</p>
