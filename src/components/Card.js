8a925315d0be2f36aa0636936324126cf9fae99f
Mona Khorasani <mona.khorasani@medieinstitutet.se>
2021-03-08 11:19:43 +0100

    a bit styling

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,34 +1,41 @@
 import React, { useContext }from 'react';
 import { BasketContext } from '../contexts/BasketContext';
 import { CarContext } from '../contexts/CarContext'
+import styles from "../styles/CardsWrapperStyles.module.css";
+
 
 // looping over car items(got info from BasketContext), map them to be able to render every chosen item including it's details
 const Card = () => {
     const {customerBasket} = useContext(BasketContext);
 
     return ( 
-        <div className="cardcontainer">  
-        {customerBasket.map((car, i) => (
-            <div className="details-wrapper">
-                <div>
-                    <img src={`/assets/car-pictures/${car.make}-${car.model}-${car.year}.jpg`} alt={`picture of ${car.make} ${car.model}`} />
-                </div>  
-                <div>
-                    <p>Make: {car.make}</p>
-                    <p>Model: {car.model}</p>
-                    <p>Year: {car.year}</p>
-                </div>
-                <p>Short description: {car.descShort}</p>
-                <div>
-                    <p>Price: {car.price}</p>
-                    <button  className="trash-btn">
-                        <i className="fas fa-trash"></i>
-                    </button>
-                </div>
+        <div className={styles.cardWrapperContainer}>
+            <div className={styles.cardWrapper}>  
+                <h2 className={styles.cardHeading}>Current item/s in your basket</h2>
+                {customerBasket.map((car, i) => (
+                    <div className={styles.flex}>
+                        <div className={styles.carImageContainer}>
+                            <img className={styles.carImage}
+                                src={`/assets/car-pictures/${car.make}-${car.model}-${car.year}.jpg`} 
+                                alt={`picture of ${car.make} ${car.model}`} 
+                            />
+                        </div>
+                        <div> 
+                            <span>Make: {car.make} Model: {car.model}</span>     
+                            <p>Year: {car.year}</p>
+                        </div>
+                        <p>Short description: {car.descShort}</p>
+                        <div>
+                            <p>Price: {car.price}</p>
+                            <button  className="trash-btn">
+                                <i className="fas fa-trash"></i>
+                            </button>
+                        </div>
+                    </div>
+                ))}
             </div>
-        ))}
-    </div>
-);
+        </div>
+    );
 }
 
 export default Card;
