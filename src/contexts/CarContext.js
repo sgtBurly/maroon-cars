0258e8f4e6f068685d3c3fde51f9383b8f9ba5ad
frida stenberg <fridastenberg97@hotmail.com>
2021-03-16 13:34:17 +0100

    added toLoweCase
    Frida & Christina

--- src/contexts/CarContext.js
+++ src/contexts/CarContext.js
@@ -11,13 +11,15 @@ const CarContextProvider = (props) => {
     setCars(require("../json/cars.json"));
   }, []);
 
-  const searchWord = 'vo';
+  const searchWord = 'VOLVO';
 
   const filterTextInput = (array, searchInput) => {
+
+
     console.log('Before filtering', filteredCars)
     // For every object, get all keys for every object and search for the textInput from the user.
     // For values containing numbers/boolean need to be stringified using toString()
-    const tempArray =  array.filter(obj => Object.keys(obj).some(key => key === 'year' || key === 'miles' || key === 'price' || key === 'discount' ? obj[key].toString().includes(searchInput) : obj[key].includes(searchInput)));
+    const tempArray =  array.filter(obj => Object.keys(obj).some(key => key === 'year' || key === 'miles' || key === 'price' || key === 'discount' ? obj[key].toString().includes(searchInput.toLowerCase()) : obj[key].toLowerCase().includes(searchInput.toLowerCase())));
     //const tempArray =  filteredCars.filter(obj => obj['miles'].toString().includes(searchInput));
 
     console.log('In filterTextInput:', tempArray);
