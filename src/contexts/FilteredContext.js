3e77752d97802a41883db4f3d77c898ae84453e9
Cedrik von Heiroth <cedrikvonheiroth@gmail.com>
2021-03-16 13:24:28 +0100

    small fix

--- src/contexts/FilteredContext.js
+++ src/contexts/FilteredContext.js
@@ -15,19 +15,20 @@ const FilteredContextProvider = (props) => {
     let maxMiles = 13000;
     let minYear = 2005;
     let maxYear = 2008;
-    // let filterModel = "Montana SV6";
-    // let filterMake =  "Pontiac";
+    let filterModel = "Montana SV6";
+    let filterMake =  "Pontiac"
     
     //Add the cars that are pass the filtration process
     let filteredCars = cars.filter(car => car.price >= minPrice && 
         car.price <= maxPrice && car.miles >= minMiles &&
-        car.miles <= maxMiles && 
-        car.year >= minYear && car.year <= maxYear 
-        // && car.make === filterMake || car.make === "" &&
-        // car.model === filterModel || car.model === ""
-       );
-        console.log(filteredCars)
+        car.miles <= maxMiles && car.year >= minYear && 
+        car.year <= maxYear 
+        && 
+        car.make === filterMake || car.make === "" &&
+        car.model === filterModel || car.model === ""))
     }  
+    filterCars();
+
     useEffect(() => {
         console.log("This is the filteredCars array:")
         filterCars();
