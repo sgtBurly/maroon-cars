71f1bf47e6816b5413dd253065cb74c78ebd5632
August <augustraro112@hotmail.com>
2021-03-16 13:20:31 +0100

    Fixed bugg again

--- src/contexts/FilteredContext.js
+++ src/contexts/FilteredContext.js
@@ -4,33 +4,34 @@ import { CarContext } from './CarContext';
 export const FilteredContext = createContext();
 
 const FilteredContextProvider = (props) => {
-  const { filteredCars, setFilteredCars } = useState([]);
-  const { cars } = useContext(CarContext);
+   
+    const { cars } = useContext(CarContext);
   
-  //Filter all the cars function that runs on form-submit
-  const filterCars = () => {
+    //Filter all the cars function that runs on form-submit
+    const filterCars = () => {
     let minPrice = 299379;
     let maxPrice = 300000;
     let minMiles = 12326;
     let maxMiles = 13000;
     let minYear = 2005;
     let maxYear = 2008;
-    let filterModel = "";
-    let filterMake =  ""
+    // let filterModel = "Montana SV6";
+    // let filterMake =  "Pontiac";
     
     //Add the cars that are pass the filtration process
-    setFilteredCars(cars.filter(car => car.price >= minPrice && 
+    let filteredCars = cars.filter(car => car.price >= minPrice && 
         car.price <= maxPrice && car.miles >= minMiles &&
-        car.miles <= maxMiles && car.year >= minYear && 
-        car.year <= maxYear && 
-        car.make === filterMake || car.make === "" &&
-        car.model === filterModel || car.model === ""))
-    }  
-    //filterCars();
-
-    useEffect(() => {
+        car.miles <= maxMiles && 
+        car.year >= minYear && car.year <= maxYear 
+        // && car.make === filterMake || car.make === "" &&
+        // car.model === filterModel || car.model === ""
+       );
         console.log(filteredCars)
-      }, [filteredCars])
+    }  
+    useEffect(() => {
+        console.log("This is the filteredCars array:")
+        filterCars();
+      }, [cars])
       const values = {
         cars
       }
