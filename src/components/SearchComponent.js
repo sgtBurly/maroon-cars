aa6d83be212d2fa1132a7a1e98e70662b4d6d20f
Christina <chrasse@hotmail.com>
2021-03-17 11:49:43 +0100

    Changed min/max and added some margins

--- src/components/SearchComponent.js
+++ src/components/SearchComponent.js
@@ -18,9 +18,9 @@ const SearchComponent = () => {
         console.log("Filters have been applyed");
     }
 
-    const [price, setPrice] = useState([null, null]);
-    const [miles, setMiles] = useState([null, null]);
-    const [year, setYear] = useState([null, null]);
+    const [price, setPrice] = useState([50000, 800000]);
+    const [miles, setMiles] = useState([0, 100000]);
+    const [year, setYear] = useState([1970, 2021]);
 
     const handlePriceChange = (e, newValue) => {
         setPrice(newValue);
@@ -37,14 +37,14 @@ const SearchComponent = () => {
         console.log("this is miles: ", miles);
     }
 
-    return ( 
+    return (
         <div className={styles.searchComponent}>
-            <form 
+            <form
             onSubmit={handleSearch}
             className={styles.formContainer}
             >
                 <div className={styles.searchAndFilterWrapper}>
-                    <input 
+                    <input
                     type="text"
                     placeholder='Search...'
                     className={styles.searchInput}
@@ -59,13 +59,13 @@ const SearchComponent = () => {
                         <div className={styles.filterSlider}>
                             <Slider
                                 value={price}
-                                min={0}
-                                max={1000000}
-                                valueLabelDisplay="auto"
+                                min={50000}
+                                max={800000}
+                                valueLabelDisplay="on"
                                 aria-labelledby="range-slider"
                                 onChange={handlePriceChange}
                             />
-                        </div> 
+                        </div>
                     </div>
                     <div className={styles.sliderWrapper}>
                         <div className={styles.labelWrapper}>
@@ -76,7 +76,7 @@ const SearchComponent = () => {
                                 value={miles}
                                 min={0}
                                 max={100000}
-                                valueLabelDisplay="auto"
+                                valueLabelDisplay="on"
                                 aria-labelledby="range-slider"
                                 onChange={handleMilesChange}
                             />
@@ -90,76 +90,35 @@ const SearchComponent = () => {
                             <div className={styles.filterSlider}>
                                 <Slider
                                     value={year}
-                                    min={1930}
-                                    max={2020}
-                                    valueLabelDisplay="auto"
+                                    min={1970}
+                                    max={2021}
+                                    valueLabelDisplay="on"
                                     aria-labelledby="range-slider"
                                     onChange={handleYearChange}
                                 />
                             </div>
-                        </div>                 
+                        </div>
                         <div>
                             <div>
                                 <label >Make:</label>
                                 <select name="make" id="make">
                                     <option value="volvo">Volvo</option>
                                 </select>
-                            </div> 
+                            </div>
                             <div>
                                 <label  >Model:</label>
                                 <select name="model" id="model">
                                     <option value="V40">V40</option>
                                 </select>
-                            </div> 
+                            </div>
                             <button type="button" onClick={handleClear}>Clear filter</button>
                             <button type="submit" onClick={handleApply}>Apply filter</button>
                         </div>
                     </div>
-                </div>    
+                </div>
             </form>
         </div>
      );
     }
 
 export default SearchComponent;
-
-/*
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Typography from '@material-ui/core/Typography';
-import Slider from '@material-ui/core/Slider';
-
-const useStyles = makeStyles({
-  root: {
-    width: 300,
-  },
-});
-
-function valuetext(value) {
-  return `${value}°C`;
-}
-
-export default function RangeSlider() {
-  const classes = useStyles();
-  const [value, setValue] = React.useState([20, 37]);
-
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
-
-  return (
-    <div className={classes.root}>
-      <Typography id="range-slider" gutterBottom>
-        Temperature range
-      </Typography>
-      <Slider
-        value={value}
-        onChange={handleChange}
-        valueLabelDisplay="auto"
-        aria-labelledby="range-slider"
-        getAriaValueText={valuetext}
-      />
-    </div>
-  );
-}
-*/
