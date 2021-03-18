770e624e765e9e5ade1b06836f427cb821a372e3
Johan Markström <johan.markstrom@gmail.com>
2021-03-18 10:14:00 +0100

    removed invalid code

--- src/pages/membershipPage.js
+++ src/pages/membershipPage.js
@@ -1,10 +1,8 @@
 import react from 'react';
-import registerComponent from "../components/registerComponent"
 
 const membershipPage = () => {
     return ( 
         <div className="membershipPage">
-            <registerComponent /> 
         </div>
      );
 }
