1577e94aeb2dfa96cfeeeb6d0dfe1b6e3e3d4dc4
Christina <chrasse@hotmail.com>
2021-03-18 10:20:14 +0100

    Added route for Memb.page and changed file name

---
+++ src/pages/Membership.js
@@ -0,0 +1,11 @@
+import react from 'react';
+
+const Membership = () => {
+    return (
+        <div className="membership">
+            <h1>Welcome to the Membership page!</h1>
+        </div>
+     );
+}
+
+export default Membership;
