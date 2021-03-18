3be4cbe1c4c4175cb9fb2b119dac78cd398977ee
Johan Markström <johan.markstrom@gmail.com>
2021-03-18 11:33:08 +0100

    imported component to membership page
    Added functionality to form, and functions with console logs

--- src/components/RegisterComponent.js
+++ src/components/RegisterComponent.js
@@ -1,19 +1,85 @@
 
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import styles from '../styles/RegisterComponentStyles.module.css'
 
 const RegisterComponent = () => {
 
-    const [password, setPassword] = useState("")
+    const [Password, setPassword] = useState("")
+    const [ConfirmPassword, setConfirmPassword] = useState("")
+    const [ FirstName, setFirstName ] = useState("");
+    const [ LastName, setLastName ] = useState("");
+    const [ Address, setAddress ] = useState("");
+    const [ City, setCity ] = useState("");
+    const [ ZipCode, setZipCode ] = useState("");
+    const [ Country, setCountry ] = useState("");
+    const [ Email, setEmail] = useState("");
+
+    const updateUserFName = e => {
+        setFirstName(e.target.value);
+    }
+    const updateUserLName = e => {
+        setLastName(e.target.value);
+    }
+    const updateUserEmail = e => {
+        setEmail(e.target.value);
+    }
+    const updateUserAddress = e => {
+    setAddress(e.target.value);
+    }
+    const updateUserCity = e => {
+        setCity(e.target.value);
+    }
+    const updateUserZipCode = e => {
+        setZipCode(e.target.value);
+    }
+    const updateUserCountry = e => {
+        setCountry(e.target.value);
+    }
 
     const handleUserPassword = (e) => {
         setPassword(e.target.value);
-        console.log("THis is user password: ", password)
+        console.log("This is user password: ", Password)
     }
+
+
+    const HandleUserComfirmPassword = (e) => {
+        setConfirmPassword(e.target.value);
+        console.log("user confirmed password: ", ConfirmPassword);
+    }
+
+    const handleAccountSubmit = (e) => {
+
+        e.preventDeafault();
+        const userCredentials = {
+            Password,
+            FirstName,
+            LastName,
+            Address,
+            City,
+            ZipCode,
+            Country,
+            Email
+        }
+        console.log("Form submitted");
+
+        //send data to membershipContext, function not created yet.
+        console.log(userCredentials);
+    }
+
+
+    useEffect( () => {
+
+        if (Password === ConfirmPassword){
+            console.log("success");
+        }else {
+            console.log("denied");
+        }
+
+    }, [handleAccountSubmit]);
 
     return ( 
         <div>
-            <form className={styles.Form} onSubmit={handleSubmit}>
+            <form className={styles.Form} onSubmit={handleAccountSubmit}>
                     <div className={styles.ContactInfo}>
                         <div className={styles.inputWrapper}>
                             <input className={`${styles.textInput} ${styles.eMail_input}`} type="text" onChange={updateUserEmail} placeholder="Email..." required />
@@ -32,10 +98,10 @@ const RegisterComponent = () => {
                         </div>
                         <div>
                         <input className={styles.textInput} type="password" onChange={handleUserPassword} placeholder="Password..." required/>
-                        <input className={styles.textInput} type="password" onChange={handleUserComfirmPassword} placeholder="Confirm password..." required/>
+                        <input className={styles.textInput} type="password" onChange={HandleUserComfirmPassword} placeholder="Confirm password..." required/>
                         </div>
                     </div>
-                    <button type="submit" className={styles.completeContactFormBtn}>Create account</button>
+                    <button className={styles.completeContactFormBtn}>Create account</button>
                 </form>
         </div>
      );
