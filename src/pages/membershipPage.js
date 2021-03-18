eed4710235b8afbb4458bcc00d63dadc594d0726
Johan Markström <johan.markstrom@gmail.com>
2021-03-18 10:13:12 +0100

    created membershipPage

---
+++ src/pages/membershipPage.js
@@ -0,0 +1,12 @@
+import react from 'react';
+import registerComponent from "../components/registerComponent"
+
+const membershipPage = () => {
+    return ( 
+        <div className="membershipPage">
+            <registerComponent /> 
+        </div>
+     );
+}
+ 
+export default membershipPage;
