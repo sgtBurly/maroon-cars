d587e0410145a3e63c1148730c83c6f111e703e2
frida stenberg <fridastenberg97@hotmail.com>
2021-03-22 11:17:25 +0100

    added func for saving logIn user data
    frida & johan & mona

--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -1,13 +1,39 @@
+import { useState } from "react";
 import styles from "../styles/LogInStyles.module.css";
 
 function LogIn() {
+
+    const [userPassword, setUserPassword] = useState("");
+    const [userEmail, setUserEmail] = useState("");
+
+
+    const updateUserPassword = (e) => {
+        setUserPassword(e.target.value);
+    }
+    const updateUserEmail = (e) => {
+        setUserEmail(e.target.value);
+    }
+
+    const handleLogIn = (e) => {
+        e.preventDefault();
+
+        const user = {
+            userPassword,
+            userEmail
+        }
+
+        //hittepå func för att skicka datan till context
+        //transferLogInData(user);
+    }
+
+
     return (
         <div className={styles.logIn}>
             <div className={styles.logInWrapper}>
-                <form className={styles.form}>
+                <form className={styles.form} onSubmit={handleLogIn}>
                     <h2 className={styles.form_title}>Log In</h2>
-                    <input type="text" placeholder="Enter Username" name="uname" className={styles.input} required/>
-                    <input type="password" placeholder="Enter Password" name="psw" className={styles.input} required/>
+                    <input type="text" placeholder="Enter Username" name="uname" onChange={updateUserEmail} className={styles.input} required/>
+                    <input type="password" placeholder="Enter Password" name="psw" onChange={updateUserPassword} className={styles.input} required/>
                     <button className={styles.btn}>Log In</button>
                 </form>
             </div>
