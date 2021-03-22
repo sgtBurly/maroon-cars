5b904fcb431fd6e365c74fe10613218a2e9cf2d3
August <augustraro112@hotmail.com>
2021-03-22 09:51:47 +0100

    Removed whitespace

--- src/contexts/MemberContext.js
+++ src/contexts/MemberContext.js
@@ -5,25 +5,22 @@ export function MemberProvider(props){
 
     const [Members, setMembers] = useState([{test: "test"}]);
     const [loggedInMember, setLoggedInMember] = useState({})
-
     const transferUserData = (newUser) => {
         setMembers([...Members, {...newUser}]);
     };
-
     //Function that checks if the member has correct user-input to be logged in
     const loginFunc = (memberInput) => {
         const successfulLogin = Members.filter(member => member.username === memberInput.username && member.password === memberInput.password);
         if (successfulLogin) {
               //Sets the logged in member
               setLoggedInMember(successfulLogin[0]);
+           
         }
         else {
             alert("INVALID USERNAME OR PASSWORD");
         }
-
-      
     }
-
+    
     useEffect(() => {
         console.log("this is Members: ", Members);
     }, [Members]);
