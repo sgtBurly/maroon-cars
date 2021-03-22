2b6ee07de7dfa123646389ab23f8765c1e175ce0
Johan Markström <johan.markstrom@gmail.com>
2021-03-22 16:03:56 +0100

    added conditional rendering to payment page
    Mona, Frida

--- src/pages/PaymentPage.js
+++ src/pages/PaymentPage.js
@@ -2,24 +2,34 @@ import React, { useContext, useEffect } from "react";
 import PaymentForm from "../components/PaymentForm";
 import EmptyBasket from "../components/EmptyBasket";
 import { BasketContext } from '../contexts/BasketContext';
+import { MemberContext } from '../contexts/MemberContext';
 import Card from '../components/Card'
-
+import LogIn from '../components/LogIn';
+import RegisterComponent from '../components/RegisterComponent';
 
 const PaymentPage = () => {
+
+    // localStorage.clear();
     const {customerBasket} = useContext(BasketContext);
+    const {loggedInMember} = useContext(MemberContext);
 
     useEffect(() => window.scrollTo(0,0), []);
 
-    if (customerBasket.length < 1) {
-        return <EmptyBasket />
-    } else {
+    if (loggedInMember.email){
         return (
-            <div className="PaymentPage">
-                <Card />
-                <PaymentForm />
-            </div>
+        <div>
+            <RegisterComponent /> 
+            <LogIn />
+        </div>
+        )
+    } else if(customerBasket.length < 1) {
+        return (
+            <EmptyBasket />
         );
+    } else {
+        return <Card />
     }
+
 }
 
 export default PaymentPage;
