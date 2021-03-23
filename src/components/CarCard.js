a3bee212a30d6df409f2158ede451da7f5598af1
Cedrik von Heiroth <cedrikvonheiroth@gmail.com>
2021-03-23 10:31:57 +0100

    Added sale logo to the discounted cars

--- src/components/CarCard.js
+++ src/components/CarCard.js
@@ -6,22 +6,30 @@ import { useHistory } from "react-router-dom";
 //receiving props from parent CardsWrapper
 function CarCard(props) {
     const { addToBasket } = useContext(BasketContext);
-//Saving the history hook in a variable
-const historyHook = useHistory();
+    //Saving the history hook in a variable
+    const historyHook = useHistory();
 
-const clickToRender = () => {
-    //Using the cars unique vin-number to push to new route with the historyHook variable
-    historyHook.push(`/details/${props.data.vin}`)
-}
+    const clickToRender = () => {
+        //Using the cars unique vin-number to push to new route with the historyHook variable
+        historyHook.push(`/details/${props.data.vin}`)
+    }
 
-const handleClick = (e) => {
-    e.stopPropagation();
-    addToBasket(props.data);
-}
+    const handleClick = (e) => {
+        e.stopPropagation();
+        addToBasket(props.data);
+    }
+    
     return (
         <div className="col-lg-4 col-sm-6 mb-3">
             <div className={`${CarCardStyles.carCard} h-100`} onClick={clickToRender}>
+
+            {/* Checks if car on discount, if so, then put a sale icon on the picture */}
+            {props.data.discount && <div className={CarCardStyles.saleIcon} >
+            <img src={`/assets/sale-icon.png`} alt="sale icon"/>
+            </div>}
+
                 <img className={`${CarCardStyles.img} card-img-top`} src={`../assets/car-pictures/${props.data.make}-${props.data.model}-${props.data.year}.jpg`} alt={props.data.model}/>
+
                 <div className="card-body">
                     <h5 className="card-title"><span>{props.data.make}  {props.data.model}</span></h5>
                     <p className={CarCardStyles.price}><span>Price: </span>${props.data.price}</p>
