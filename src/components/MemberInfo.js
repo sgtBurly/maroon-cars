3b8bd086b9db0f45e6e45bfe19fd06fa85d22807
Christina <chrasse@hotmail.com>
2021-03-24 11:40:42 +0100

    Styling

--- src/components/MemberInfo.js
+++ src/components/MemberInfo.js
@@ -1,19 +1,25 @@
 import { useContext } from "react";
 import { MemberContext } from "../contexts/MemberContext";
+import styles from '../styles/MemberInfo.module.css'
 
 const MemberAddress = () => {
 
   const { loggedInMember } = useContext(MemberContext);
 
   return (
-    <div className='memberaddress'>
+    <div className={styles.memberInfo}>
       <h3>Personal info:</h3>
-      <div>
-          <p>Name: <span>{`${loggedInMember.firstName} ${loggedInMember.lastName}`}</span></p>
-          <p>E-mail: <span>{loggedInMember.email}</span></p>
-          <p>Address: <span>{loggedInMember.address}</span></p>
-          <p>Zipcode: <span>{loggedInMember.zipCode}</span></p>
-          <p>City: <span>{loggedInMember.city}</span></p>
+      <hr />
+      <div className={styles.memberInfoWrapper}>
+        <div className={styles.memberInfoPart1}>
+          <p>Name:  <span>{`${loggedInMember.firstName} ${loggedInMember.lastName}`}</span></p>
+          <p>E-mail:  <span>{loggedInMember.email}</span></p>
+        </div>
+        <div className={styles.memberInfoPart2}>
+          <p>Address:  <span>{loggedInMember.address}</span></p>
+          <p>Zipcode:  <span>{loggedInMember.zipCode}</span></p>
+          <p>City:  <span>{loggedInMember.city}</span></p>
+        </div>
       </div>
     </div>
   );
